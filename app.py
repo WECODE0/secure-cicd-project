b6908c42040850e74e0b58fda494fe71b9ad578f
WECODE0 <ar4201004@gmail.com>
2026-06-10 13:32:32 +0000

    Demo pipeline run

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Secure CI/CD Pipeline App is running!"
+    return "Secure CI/CD Pipeline App is running!2"
 
 @app.route("/health")
 def health():
